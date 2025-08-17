7f7d646b296c2a3246a018b46e45ec2ba75bcdc8
Esme577 <esmeraldaquezada57@gmail.com>
2025-08-17 14:48:26 -0700

    sonnected supabse

--- Frontend/src/client.js
+++ Frontend/src/client.js
@@ -1,8 +1,8 @@
-// import { createClient } from "@supabase/supabase-js";
+import { createClient } from "@supabase/supabase-js";
 
-// const supabase = createClient(
-//   "<Project-URL>",
-//   import.meta.env.VITE_SUPABASE_ANON_KEY,
-// );
+const supabase = createClient(
+  "https://nfxryxdpolhaerrhchvk.supabase.co",
+  import.meta.env.VITE_SUPABASE_ANON_KEY,
+);
 
-// export default supabase;
+export default supabase;
