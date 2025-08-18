4a61f6e3f32b6a7f853bb0e0d06f1ebe25d18d8f
Esme577 <esmeraldaquezada57@gmail.com>
2025-08-17 19:29:31 -0700

    added the html

--- Frontend/src/pages/aboutUs.jsx
+++ Frontend/src/pages/aboutUs.jsx
@@ -3,8 +3,23 @@ import React from 'react';
 const AboutUs = () => {
   return (
     <>
+
     
      <h1> Meet Our team</h1>
+     <div>
+      <div className='dev1'>
+        <h2 className='devname'> Erick </h2>
+        <p>story</p>
+      </div>
+      <div className='dev2'>
+        <h2 className='devname'> Shanea </h2>
+        <p>story</p>
+      </div>
+      <div className='dev3'>
+        <h2 className='devname'> Esmeralda </h2>
+        <p>story</p>
+      </div>
+     </div>
 
     </>
   );
