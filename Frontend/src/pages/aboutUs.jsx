bdb514ed416b65e356bc5a475bae1bb1b2480bd8
Esme577 <esmeraldaquezada57@gmail.com>
2025-08-17 23:37:06 -0700

    developed the pages just need to be styled

--- Frontend/src/pages/aboutUs.jsx
+++ Frontend/src/pages/aboutUs.jsx
@@ -1,25 +1,44 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+
 
 const AboutUs = () => {
   return (
     <>
 
-    
-     <h1> Meet Our team</h1>
-     <div>
-      <div className='dev1'>
-        <h2 className='devname'> Erick </h2>
-        <p>story</p>
-      </div>
-      <div className='dev2'>
-        <h2 className='devname'> Shanea </h2>
-        <p>story</p>
-      </div>
-      <div className='dev3'>
-        <h2 className='devname'> Esmeralda </h2>
-        <p>story</p>
-      </div>
+    <div>
+      <div className='aboutTitle'>
+        <h1> We MINIMIZE HUNGER AND FOOD WASTE</h1>
      </div>
+
+     <div className='aboutimg'> 
+      <img src="/pantrypalimg.png"
+       alt="Pantry bal image a smilling paper bag with a baguette and lettice leaf sticking out" />
+    </div>
+
+     <div className='aboutPantryStatement'>
+
+      <h2>About PantryPal</h2>
+
+      <p>PantryPal started as a simple idea from one of our three developers, Shanea, who had firsthand experience working with food pantries and witnessed the urgent need for food access in her community.</p>
+
+      <break></break>
+
+      <p>After a quick conversation, the other two developers, Erick and Esmeralda, jumped on board. Together, we set out to create a single-page application designed to bridge the gap between grocery stores and local communities getting food that can’t be accepted by traditional food banks directly into the hands of the people who need it most.</p>
+
+      <break></break>
+
+      <p> PantryPal isn’t just an app, it’s a movement to reduce food waste while fighting food insecurity, one connection at a time.</p>
+
+      <break></break>
+
+     </div>
+     <div>
+      <button className="btn-developers" onClick={event =>  window.location.href='/developers'}  >Meet The Developers</button>
+     </div>
+    </div>
+
 
     </>
   );
