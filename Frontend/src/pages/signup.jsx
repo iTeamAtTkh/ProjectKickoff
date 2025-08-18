f94f33d2de84fa80683ef18c1798b943316a871d
Esme577 <esmeraldaquezada57@gmail.com>
2025-08-17 19:29:56 -0700

    added ebt and name to the form

--- Frontend/src/pages/signup.jsx
+++ Frontend/src/pages/signup.jsx
@@ -13,7 +13,9 @@ const [alert, showAlert] = useState({
     const { register, handleSubmit } = useForm({
     defaultValues: {
       username: "",
+      fullName: "",
       email: "",
+      ebtNumber: "",
       snapNumber: "",
       password: ""
     }
@@ -65,8 +67,16 @@ function SignUpForm () {
       <input id="username" type="username" className="input input-bordered w-full" { ...register("username") } />
     </div>
     <div>
+      <label htmlFor="fullName" className="block text-sm font-medium text-gray-700">Name</label>
+      <input id="fullName" type="name" className="input input-bordered w-full" { ...register("fullName") } />
+    </div>
+    <div>
       <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email</label>
       <input id="email" type="email" className="input input-bordered w-full" { ...register("email") } />
+    </div>
+  <div>
+      <label htmlFor="ebtNumber" className="block text-sm font-medium text-gray-700">Ebt Number</label>
+      <input id="ebt" type="text" className="input input-bordered w-full" { ...register("ebtNumber") } />
     </div>
     <div>
       <label htmlFor="snapNumber" className="block text-sm font-medium text-gray-700">Snap Number</label>
